8b0dfbadd0517e64fb5655bead027c2187105723
lukss127 <malet@hotmail.de>
2016-10-19 15:38:02 +0200

    Funktion Draw

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,8 +10,24 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Snake");
+            int x1 = 1;
+            int y1 = 3;
+            char sim1 = '*';
+
+            Draw(x1, y1, sim1);
+
+            int x2 = 4;
+            int y2 = 5;
+            char sim2 = '#';
+
+            Draw(x2, y2, sim2);
+
             Console.ReadLine();
+        }
+        static void Draw( int x, int y, char sim)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sim);
         }
     }
 }
