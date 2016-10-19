634493f27320477a423da78ad7c29f453229838a
lukss127 <malet@hotmail.de>
2016-10-19 16:03:18 +0200

    Classe Point

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,24 +10,22 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char sim1 = '*';
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sim = '*';
 
-            Draw(x1, y1, sim1);
+            p1.Draw();
 
-            int x2 = 4;
-            int y2 = 5;
-            char sim2 = '#';
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sim = '#';
 
-            Draw(x2, y2, sim2);
+            p2.Draw();
 
             Console.ReadLine();
         }
-        static void Draw( int x, int y, char sim)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sim);
-        }
+        
     }
 }
